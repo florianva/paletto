ff143260702042dd059ab3e4e7ca15cc99a7c3e0
Geoffrey <geoffrey.pruvost@hotmail.com>
2016-11-07 11:59:54 +0100

    ajout de la story 1

--- src/Engine.js
+++ src/Engine.js
@@ -3,6 +3,79 @@
 var Engine = function () {
 
 // private attributes and methods
+    var tableau = [];
+
+// public attributes
+    this.enumPion = {NOIR: "noir", VERT: "vert", BLEU: "bleu", BLANC: "blanc", JAUNE: "jaune", ROUGE: "rouge"};
 
 // public methods
+    this.initPlateau = function () {
+        var tab = [this.enumPion.NOIR, this.enumPion.VERT, this.enumPion.BLANC, this.enumPion.BLEU, this.enumPion.ROUGE, this.enumPion.BLANC,
+            this.enumPion.JAUNE, this.enumPion.BLANC, this.enumPion.VERT, this.enumPion.ROUGE, this.enumPion.JAUNE, this.enumPion.BLEU,
+            this.enumPion.BLEU, this.enumPion.JAUNE, this.enumPion.BLEU, this.enumPion.BLANC, this.enumPion.NOIR, this.enumPion.ROUGE,
+            this.enumPion.ROUGE, this.enumPion.NOIR, this.enumPion.ROUGE, this.enumPion.VERT, this.enumPion.BLEU, this.enumPion.BLANC,
+            this.enumPion.BLANC, this.enumPion.VERT, this.enumPion.JAUNE, this.enumPion.NOIR, this.enumPion.JAUNE, this.enumPion.VERT,
+            this.enumPion.JAUNE, this.enumPion.BLEU, this.enumPion.NOIR, this.enumPion.ROUGE, this.enumPion.VERT, this.enumPion.NOIR];
+
+        var parcourTab = 0;
+        for (var i = 0; i < 6; i++) {
+            tableau[i] = [];
+            for (var j = 0; j < 6; j++) {
+                tableau[i][j] = tab[parcourTab];
+                parcourTab++;
+            }
+        }
+    };
+
+    this.getTab = function(){
+        return tableau;
+    }
+
+    var testCote = function (direction, i, j) {
+        var pion = tableau[i][j];
+        if (direction == "d") {
+            if (pion != tableau[i + 1][j])
+                return true;
+            else
+                return false;
+        } else if (direction == "g") {
+            if (pion != tableau[i - 1][j])
+                return true;
+            else
+                return false;
+        } else if (direction == "h") {
+            if (pion != tableau[i][j - 1])
+                return true;
+            else
+                return false;
+        } else if (direction == "b") {
+            if (pion != tableau[i][j + 1])
+                return true;
+            else
+                return false;
+        }
+    };
+
+    this.testJuxta = function (i, j) {
+        if (i > 0 && i < 5) {
+            return testCote("g", i, j) && testCote("d", i, j);
+        } else {
+            if (i == 0) {
+                return testCote("d", i, j);
+            } else {
+                return testCote("g", i, j);
+            }
+        }
+
+        if (j > 0 && j < 5) {
+            return testCote("h", i, j) && testCote("b", i, j);
+        } else {
+            if (j == 0) {
+                return testCote("b", i, j);
+            } else {
+                return testCote("h", i, j);
+            }
+        }
+
+    }
 };
--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ PalettoTestCase.prototype.testStory1 = function () {
     for(var i=0; i<6; i++){
         for(var j=0; j<6; j++){
             var t = e.getTab()[i][j];
-            assertTrue(t.testJuxta);
+            assertTrue(e.testJuxta(i,j));
         }
     }
 
