6d278fc26283268e75826689249668fc41f59a43
florian.vansteene@gmail.com <florian.vansteene@gmail.com>
2016-11-07 11:49:35 +0100

    Ajout du test de la story 1

--- test/test.js
+++ test/test.js
@@ -2,5 +2,16 @@
 
 var PalettoTestCase = TestCase("PalettoTestCase");
 
+var e = new Engine();
+
 PalettoTestCase.prototype.testStory1 = function () {
+
+    e.initPlateau();
+    for(var i=0; i<6; i++){
+        for(var j=0; j<6; j++){
+            var t = e.getTab()[i][j];
+            assertTrue(t.testJuxta);
+        }
+    }
+
 };
