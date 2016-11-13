03845297f4f51ea80fb2fb53499d7a7dfb9a033c
Geoffrey <geoffrey.pruvost@hotmail.com>
2016-11-13 19:23:58 +0100

    ajout test story6

--- test/test.js
+++ test/test.js
@@ -31,3 +31,35 @@ PalettoTestCase.prototype.testStory3 = function () {
     assertTrue(e.getNbPieces() === 35);
 
 };
+
+PalettoTestCase.prototype.testStory6 = function () {
+    e.choose(1,"A1");
+    e.choose(1,"F6");
+    e.choose(2,"B1");
+    e.choose(2,"E6");
+    e.choose(2,"F5");
+    e.choose(1,"A2");
+    e.choose(1,"A6");
+    e.choose(2,"A3");
+    e.choose(1,"A5");
+    e.choose(1,"F4");
+    e.choose(1,"F1");
+    e.choose(1,"C1");
+    e.choose(2,"E1");
+    e.choose(2,"F3");
+    e.choose(2,"D6");
+    e.choose(2,"A4");
+    e.choose(1,"D3");
+    e.choose(1,"F2");
+    e.choose(1,"B6");
+    e.choose(2,"B3");
+    e.choose(2,"E2");
+    e.choose(2,"E5");
+    e.choose(1,"B4");
+    e.choose(1,"C6");
+    e.choose(1,"D5");
+    e.choose(1,"E3");
+
+    assertTrue(e.getWinner()=="joueur1");
+
+};
